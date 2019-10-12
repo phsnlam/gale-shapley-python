4f7f1bc30266f0dddc64e846f983edbef519c658
Jake P <phsnlam@gmail.com>
2019-10-11 23:28:59 -0400

    Add files via upload

--- gs1.py
+++ gs1.py
@@ -4,63 +4,57 @@
 Created by: Jake Pham
 Date: 09/30/2019
 PURPOSE: Demonstrate the implementation of the Gale-Shapley algorithm
-Input: gsinput.txt
-Output: gsoutput.txt
-Examples:
+Input: gs1input.txt
+Output: gs2output.txt
 """
 
 import time
 import random
+import argparse
+import sys, os
 
-#Create a dictionary for the preference orders of the suitors
-suitor_rank_list = {
-    'Jake': ['Flo', 'Kat', 'Nel', 'Cait', 'Jinx', 'Anna', 'Liz', 'Ruby', 'Rose', 'Mary'],
-    'Will': ['Flo', 'Jinx', 'Kat', 'Cait', 'Ruby', 'Mary', 'Anna', 'Nel', 'Rose', 'Liz'],
-    'Zack': ['Rose', 'Flo', 'Nel', 'Ruby', 'Anna', 'Mary', 'Cait', 'Kat', 'Liz', 'Jinx'],
-    'Alex': ['Cait', 'Anna', 'Ruby', 'Nel', 'Jinx', 'Mary', 'Liz', 'Flo', 'Rose', 'Kat'],
-    'Kyle': ['Liz', 'Ruby', 'Kat', 'Jinx', 'Mary', 'Flo', 'Anna', 'Rose', 'Nel', 'Cait'],
-    'Sam': ['Nel', 'Ruby', 'Liz', 'Anna', 'Kat', 'Mary', 'Flo', 'Jinx', 'Cait', 'Rose'],
-    'Joe': ['Ruby', 'Flo', 'Kat', 'Jinx', 'Anna', 'Rose', 'Liz', 'Nel', 'Cait', 'Mary'],
-    'Josh': ['Nel', 'Liz', 'Kat', 'Mary', 'Jinx', 'Anna', 'Ruby', 'Rose', 'Cait', 'Flo'],
-    'Mike': ['Kat', 'Anna', 'Flo', 'Mary', 'Jinx', 'Ruby', 'Cait', 'Nel', 'Liz', 'Rose'],
-    'Ryan': ['Mary', 'Jinx', 'Nel', 'Flo', 'Kat', 'Rose', 'Cait', 'Liz', 'Ruby', 'Anna']
-}
+"""A few words before we start:
+This version of gs1 creates a completely random dictionary whose number
+of elements are determined by the user input, using sample random within a
+certain range to avoid duplicates within groups.
+"""
 
-#Create a dictionary for the preference orders of the girls
-girl_rank_list = {
-    'Flo': ['Jake', 'Will', 'Zack', 'Alex', 'Kyle', 'Sam', 'Joe', 'Josh', 'Mike', 'Ryan'],
-    'Kat': ['Will', 'Mike', 'Sam', 'Kyle', 'Zack', 'Jake', 'Alex', 'Joe', 'Ryan', 'Josh'],
-    'Nel': ['Kyle', 'Will', 'Ryan', 'Mike', 'Josh', 'Zack', 'Joe', 'Alex', 'Sam', 'Jake'],
-    'Cait': ['Zack', 'Joe', 'Jake', 'Will', 'Alex', 'Ryan', 'Mike', 'Josh', 'Kyle', 'Sam'],
-    'Jinx': ['Alex', 'Zack', 'Sam', 'Josh', 'Joe', 'Will', 'Kyle', 'Mike', 'Ryan', 'Jake'],
-    'Anna': ['Jake', 'Sam', 'Ryan', 'Will', 'Alex', 'Mike', 'Joe', 'Josh', 'Kyle', 'Zack'],
-    'Liz': ['Mike', 'Kyle', 'Zack', 'Jake', 'Alex', 'Will', 'Joe', 'Josh', 'Sam', 'Ryan'],
-    'Ruby': ['Ryan', 'Kyle', 'Alex', 'Will', 'Mike', 'Sam', 'Jake', 'Josh', 'Zack', 'Joe'],
-    'Rose': ['Will', 'Jake', 'Mike', 'Kyle', 'Joe', 'Zack', 'Alex', 'Ryan', 'Josh', 'Sam'],
-    'Mary': ['Joe', 'Zack', 'Jake', 'Alex', 'Ryan', 'Mike', 'Will', 'Josh', 'Kyle', 'Sam']
-}
 
 #Create an empty list that contains the engaged couples as tuples
 engaged = []
-
+    
 #Create an empty list that will later contain the suitors that are not engaged
 suitor_available = []
 
+#Initialize a dictionary for the preference orders of the suitors
+def initializeM(n):    
+    suitor_rank_list = {
+        suitor: random.sample(range(n,2*n), n) for suitor in range(0, n)
+    }
+    return suitor_rank_list
+
+#Initialize a dictionary for the preference orders of the girls
+def initializeF(n):
+    girl_rank_list = {
+        girl: random.sample(range(0,n), n) for girl in range(n, 2*n)
+    }
+    return girl_rank_list
+    
 #Initialize the list of available suitors
-def singleSuitor():
+def singleSuitor(suitor_rank_list):
     for suitor in suitor_rank_list.keys():
         suitor_available.append(suitor)
+    return suitor_available
 
 #Calls the function to match suitors with girls
-def matcher():
+def matcher(suitor_available, suitor_rank_list, girl_rank_list):
     print('- Matching will now begin:')
     while(len(suitor_available) > 0):
         for suitor in suitor_available:
-            proposal(suitor)
-            time.sleep(1)
-
+            proposal(suitor, suitor_available, suitor_rank_list, girl_rank_list)
+            
 #Where the magic happens
-def proposal(suitor):
+def proposal(suitor, suitor_available, suitor_rank_list, girl_rank_list):
     """We will loop through the list of suitors, along with their
     preferences one-by-one. This way, it is possible to ensure everyone
     has their match in the end, since the number of suitors and girls
@@ -117,7 +111,7 @@ def proposal(suitor):
     engagement[:] = []
 
 #Print the list of participating suitors and girls
-def participants():
+def participants(suitor_rank_list, girl_rank_list):
     print('- Participants:')
     for suitor in suitor_rank_list:
         print(suitor, end = ' ')
@@ -127,7 +121,7 @@ def participants():
     print('\n')
 
 #Print the preferences of all suitors and girls
-def preferences():
+def preferences(suitor_rank_list, girl_rank_list):
     print('- Preferences:')
     print('Suitors:')
     for suitor in suitor_rank_list:
@@ -144,11 +138,11 @@ def preferences():
     print('')
 
 #Print all matched pairs
-def pairing():
+def pairing(engaged):
     print('')
     print('- Pairing:')
     for pair in engaged:
-        print('{:<5s} {:^2} {:>2s}'.format(pair[0], '-', pair[1]))
+        print('{:<3} {:^2} {:>2}'.format(pair[0], '-', pair[1]))
 
 #Shuffle
 def shuffler():
@@ -157,34 +151,76 @@ def shuffler():
     for girl in girl_rank_list:
         random.shuffle(girl_rank_list[girl])
 
-#All methods go here just for easy access
-def doEverything():
-    shuffler()
-    participants()
-    preferences()
-    singleSuitor()
-    matcher()
-    pairing()
-    print('Elapsed wall clock time: {:.2f}'.format(time.perf_counter()))
-    print('Elapsed CPU time: {:.4f}'.format(time.process_time()))
-    print('Stable matchup.')
-    
-    #Empty out the list before starting new trial
+#Just for cleaning
+def empty():
     engaged[:] = []
     suitor_available[:] = []
 
+#Disable printing
+def noPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+#Allow printing
+def yesPrint():
+    sys.stdout = sys.__stdout__
+
 #The main function
 def main():
-    doEverything()
-    prompt = input('Another trial? (Y)es / (N)o \n')
-    while prompt:
-        if prompt.lower() == 'y' or prompt.lower() == 'yes':
-            doEverything()
-        
-        elif prompt.lower() == 'n' or prompt.lower() == 'no':
-            break
+    
+    #Set up the argument
+    parser = argparse.ArgumentParser(description="What to do with the file")
+    parser.add_argument('number_of_suitors',type=int)
+    
+    #Set up the flags for shortened or full output
+    parser.add_argument('-l', '--long', action="store_true",
+                        help="display all content when the program is run")
+    parser.add_argument('-s', '--short', action="store_true",
+                        help="display only time when the program is run")
+    args = parser.parse_args()
 
-        prompt = input('Another trial? (Y)es / (N)o \n')
+    #Initialize the chosen value and dictionaries
+    n = args.number_of_suitors
+    
+    suitor_rank_list = initializeM(n)
+    girl_rank_list = initializeF(n)
+
+    #Proceed as normal and print everything along the way
+    if args.long:
+        participants(suitor_rank_list, girl_rank_list)
+        preferences(suitor_rank_list, girl_rank_list)
+        singleSuitor(suitor_rank_list)
+
+        start = time.time()
+        matcher(suitor_available, suitor_rank_list, girl_rank_list)
+        end = time.time()
+        pairing(engaged)
+
+        print('')
+        print('Stable match.')
+        print('Chosen number: %s' %(n))
+        print('Elapsed wall clock time: {:.2f}'.format(end-start))
+        print('Elapsed CPU time: {:.4f}'.format(time.process_time()))  
+
+    #Suppress printing until the end if the short flag is chosen
+    elif args.short:
+        noPrint()
+    
+        participants(suitor_rank_list, girl_rank_list)
+        preferences(suitor_rank_list, girl_rank_list)
+        singleSuitor(suitor_rank_list)
+
+        start = time.time()
+        matcher(suitor_available, suitor_rank_list, girl_rank_list)
+        end = time.time()
+        pairing(engaged)
+
+        yesPrint()
+        
+        print(n, end = '    ')
+        print('{:.4f}'.format(end-start))
+
+    #Clean the lists for a potential re-run.
+    empty()
 
 if __name__ == "__main__":
     main()
